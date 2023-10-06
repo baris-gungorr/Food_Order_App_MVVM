baefca8dc651f0ea46f1a5aec74fca79e341ae57
Baris Gungor <gungorbaris41@gmail.com>
2023-10-06 14:41:17 +0300

    add login screen and firebase auth & and app colors

--- app/src/main/java/com/barisgungorr/ui/fragment/SignInFragment.kt
+++ app/src/main/java/com/barisgungorr/ui/fragment/SignInFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.barisgungorr.bootcamprecipeapp.R
 import com.barisgungorr.bootcamprecipeapp.databinding.FragmentOrderBinding
 import com.barisgungorr.bootcamprecipeapp.databinding.FragmentSignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignInFragment : Fragment() {
     private lateinit var binding: FragmentSignInBinding
-
+    private lateinit var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,8 +22,47 @@ class SignInFragment : Fragment() {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        auth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            // Navigation kodlaması
+        }
+
+        binding.buttonSignIn.setOnClickListener {
+
+            val email = binding.emailText.text.toString()
+            val pass = binding.passText.text.toString()
+
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty() ) {
+
+                    auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
+                        if (it.isSuccessful) {
+                            // Navigation geçiş kodlaması
+
+                        }else {
+                            Toast.makeText(requireContext(),it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+
+                        }
+                    }
+
+                }else {
+                    Toast.makeText(requireContext(),"Parolalar uyuşmuyor", Toast.LENGTH_SHORT).show()
+                }
+        }
+
 
 
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser != null) {
+            //Navigation kodlaması
+        }
     }
 }
--- app/src/main/java/com/barisgungorr/ui/fragment/SignUpFragment.kt
+++ app/src/main/java/com/barisgungorr/ui/fragment/SignUpFragment.kt
@@ -5,19 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.barisgungorr.bootcamprecipeapp.R
 import com.barisgungorr.bootcamprecipeapp.databinding.FragmentSignInBinding
 import com.barisgungorr.bootcamprecipeapp.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignUpFragment : Fragment() {
     private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth:FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            // Navigation kodlaması
+        }
+
+        binding.buttonSignUp.setOnClickListener {
+            val email = binding.emailText.text.toString()
+                val pass = binding.passText.text.toString()
+            val confirmPass = binding.confirmPassText.text.toString()
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+            if (pass == confirmPass) {
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        // Navigation geçiş kodlaması
+
+                    }else {
+                        Toast.makeText(requireContext(),it.exception.toString(),Toast.LENGTH_SHORT).show()
+
+
+                    }
+                }
+
+            }else {
+                Toast.makeText(requireContext(),"Parolalar uyuşmuyor",Toast.LENGTH_SHORT).show()
+            }
+            }else {
+                Toast.makeText(requireContext(),"Boş alanlara izin verilmez",Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
 
